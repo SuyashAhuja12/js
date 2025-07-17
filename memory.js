3cd3cb849cb69e734b2bc1d3c9056335eddddb65
Suyash Ahuja <121660550+SuyashAhuja12@users.noreply.github.com>
2025-07-17 14:10:48 +0530

    Delete memory.js

--- memory.js
+++
@@ -1,8 +0,0 @@
-//two tyeps stack (used for primitive), heap(used for non primitive)
-
-let myname = "suyash"
-
-let anothername = myname
-anothername = "yash"
-console.log(anothername);
-console.log(myname);
